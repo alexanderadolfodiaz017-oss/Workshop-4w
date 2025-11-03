d92a530405a15582e867320c5858d36381455222
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-03 07:27:46 -0800

    Completed the Contract class

--- src/main/java/com/dealership/Contract.java
+++ src/main/java/com/dealership/Contract.java
@@ -1,4 +1,22 @@
 package com.dealership;
 
-public class Contract {
+public abstract class Contract {
+    protected String date;
+    protected String customerName;
+    protected String customerEmail;
+    protected Vehicle vehicle;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicle = vehicle;
+    }
+
+    public abstract double getTotalPrice();
+
+    public String toString() {
+        return date + " | " + customerName + " | " + customerEmail + " | " +
+                vehicle.getMake() + " " + vehicle.getModel() + " | $" + vehicle.getPrice();
+    }
 }
