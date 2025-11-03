06cacd72b4c993b9738302541c827360da36e3d7
Alexander Diaz <adiaz1@my.yearupunited.org>
2025-11-03 07:28:56 -0800

    finished up the lease contract

--- src/main/java/com/dealership/LeaseContract.java
+++ src/main/java/com/dealership/LeaseContract.java
@@ -1,6 +1,20 @@
 package com.dealership;
 
-public class LeaseContract {
-    public LeaseContract(String today, String name, String email, Vehicle vehicle) {
+public class LeaseContract extends Contract {
+
+    public LeaseContract(String date, String name, String email, Vehicle vehicle) {
+        super(date, name, email, vehicle);
+    }
+
+    @Override
+    public double getTotalPrice() {
+        double leaseFee = vehicle.getPrice() * 0.04; // 4% lease fee
+        double monthly = (vehicle.getPrice() / 36) + leaseFee;
+        return monthly * 36; // total for 3 years
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | LEASE | Total: $" + getTotalPrice();
     }
 }
